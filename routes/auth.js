046cd109b579f400fc52bd5ecb6884727e33e776
Richard <richard.yang.tong@gmail.com>
2021-03-30 21:55:36 -0400

    Imported the register controller.

--- routes/auth.js
+++ routes/auth.js
@@ -1,4 +1,8 @@
 const express = require('express');
 const router = express.Router();
 
+const { register } = require("../controllers/auth"); 
+
+router.route("/reigster").post();
+
 module.exports = router;
